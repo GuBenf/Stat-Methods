e22f6c0de485ffdfbd284e2f9057cbe0e9885930
NicolaSommaggio <sommaggio.nicola@gmail.com>
2026-06-05 11:57:07 +0200

    new file

--- TauPhiMu/package/analysis/analysis.C
+++ TauPhiMu/package/analysis/analysis.C
@@ -11,15 +11,6 @@ using namespace std;
 
 void analysis::Loop()
 {
-
-  
-  TH1D *histo_D_M         = new TH1D("histo_D_M","",100,1.640,2.100);
-  TH1D *histo_D_M_S       = new TH1D("histo_D_M_S","",100,1.640,2.100);
-  TH1D *histo_D_time      = new TH1D("histo_D_time","",100,0,5e-12);
-  TH1D *histo_D_time_S    = new TH1D("histo_D_time_S","",100,0,5e-12);
-  TH1D *histo_D_pt        = new TH1D("histo_D_pt","",100,0,10);
-  TH1D *histo_D_pt_S      = new TH1D("histo_D_pt_S","",100,0,10);
-  TH1D *histo_h3_p_back   = new TH1D("histo_h3_p_back","",1000,0,200);
   
   if (fChain == 0) return;
 
@@ -31,13 +22,7 @@ void analysis::Loop()
     Long64_t ientry = LoadTree(jentry);
     if (ientry < 0) break;
     nb = fChain->GetEntry(jentry);   nbytes += nb;
-    // if (Cut(ientry) < 0) continue;
-    //std::cout << M0_MKK << std::endl;
-    histo_h3_p_back->Fill(h3_p,weight);
 
-    // from fit to signal MC
-    //Mean    =  1.77699   +/-   1.98392e-05 
-    //Sigma   =   0.00581298   +/-   1.49607e-05  	 (limited)
     Double_t m_tau_mc;
     Double_t m_sigma_tau_mc;
     m_tau_mc= 1.77699;
@@ -46,66 +31,10 @@ void analysis::Loop()
     if (TMath::Abs(D_M-m_tau_mc)>3*m_sigma_tau_mc)
     {
 	 
-	    if (h3_MuonID==1)
-      {
-	      //if (1){     
-	      histo_D_M->Fill(D_M) ;
-	      histo_D_time ->Fill(D_time);
-	      histo_D_pt     ->  Fill(D_pt);
-	      //std::cout << D_M << std::endl;
-	    }; 
-	    if (h3_MuonID==1 && D_time>0.25e-12 && D_pt>2.5  && D_FDt > 1e-3)
-      {
-	      //if (h3_MuonID==1 && D_time>0.25e-12 && D_pt>2.5 ){
-	      //if ( D_time>2.5e-12){
-	      histo_D_M_S->Fill(D_M) ;
-	      histo_D_time_S ->Fill(D_time);
-	      histo_D_pt_S     ->  Fill(D_pt);
-	    };
+	    
 	 
     }; //Mass blinding
      
   }; //End Loop Events
    
-   
-  //Create Canvas
-  TCanvas *c_histo_D_M = new TCanvas("c_histo_D_M","canvas histo",500,500);
-  c_histo_D_M->cd();
-  histo_D_M->SetMinimum(0);
-  histo_D_M->Draw();
-  histo_D_M_S->SetLineColor(2);
-  histo_D_M_S->Draw("same");
-
-  TCanvas *c_histo_D_time = new TCanvas("c_histo_D_time","canvas histo",500,500);
-  c_histo_D_time->cd();
-  histo_D_time->SetMinimum(0);
-  histo_D_time->Draw();
-  histo_D_time_S->SetLineColor(2);
-  histo_D_time_S->Draw("same");
-
-  TCanvas *c_histo_D_pt = new TCanvas("c_histo_D_pt","canvas histo",500,500);
-  c_histo_D_pt->cd();
-  histo_D_pt->SetMinimum(0);
-  histo_D_pt->Draw();
-  histo_D_pt_S->SetLineColor(2);
-  histo_D_pt_S->Draw("same");
-    
-  // Save canvas in .pdf and .epr format
-  c_histo_D_M->Print("./_fig/c_histo.pdf");
-  c_histo_D_M->Print("./_fig/c_histo.eps");
-  
-  //Create a new file to store histograms
-   TFile *histo_file = new TFile("./_root/histo_file.root","RECREATE","put a title");
-  //TFile *histo_file = new TFile("./_root/histo_p_back_DPLUS_PhiPi.root","RECREATE","put a title");
-  histo_file->cd();
-  histo_h3_p_back->Write();
-  //histo_file->Write();
-  histo_D_M ->Write();
-  histo_D_M_S ->Write();
-  histo_D_time->Write();
-  histo_D_time_S->Write();
-  histo_D_pt     ->Write();
-  histo_D_pt_S   ->Write(); 
-  histo_file->Close();
-     
 }
